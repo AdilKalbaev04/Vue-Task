ec08d33b4c11e6a3b0105d4450a3ec81f106c18c
“Adilgit config --global user.name “Adil <adilkalbaev2004@gmail.com>
2023-05-05 00:25:54 +0600

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Vue-Task',
+  base: '/Vue-Task',
   plugins: [vue()],
   resolve: {
     alias: {
